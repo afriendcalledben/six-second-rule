a2f773a0b1bd941a5d68f195bdac849edd2b06d2
Ben Tandy <ben@afriendcalledben.com>
2020-04-03 21:52:08 +0100

    Update

--- server.js
+++ server.js
@@ -142,7 +142,7 @@ io.on('connection', function(socket){
     } else {
       console.log('User '+id+' disconnected');
     }
-    if (user.length < 2) gameState = 0;
+    if (user.length < 2) resetGame();
     io.emit('game_state', gameState);
     io.emit('user_list_updated', JSON.stringify(users));
   }
